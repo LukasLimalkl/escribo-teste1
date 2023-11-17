eb3cf39700a963a5d8737b95470315ce772d5444
LukasLima63 <lukas.lima@portallacrei.com.br>
2023-11-17 17:49:01 -0300

    fn added

---
+++ index.js
@@ -0,0 +1,15 @@
+function calcMultiplos(num) {
+    let soma = 0
+     for (let i = 0; i < num; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            soma += i;
+        }
+    }
+    return soma;
+}
+
+
+const numeroTeste = 10;
+const resultado = calcMultiplos(numeroTeste);
+
+console.log(`O somatório de todos os números menores que ${numeroTeste} e divisíveis por 3 ou 5 é: ${resultado}`);
